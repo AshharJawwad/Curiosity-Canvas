986524e38e7099d7c5cb3a7fff8f62852a1d3b47
Ashharja <rockashhar10@gmail.com>
2024-10-31 14:00:24 +0530

    Made the  Navigation Bar responsive

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,40 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMenu, IoClose } from "react-icons/io5";
+
 import { images } from "../constants";
 
 const navItemInfo = [
-  { name: "Home" },
-  { name: "Blogs" },
-  { name: "Pricing" },
-  { name: "Faq" },
+  { name: "Home", type: "link" },
+  { name: "Blogs", type: "link" },
+  { name: "Pricing", type: "link" },
+  { name: "Faq", type: "link" },
 ];
 
-const NavItem = ({ name }) => {
+const NavItem = ({ item }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-purple-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-        /
-      </span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-purple-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <></>
+      )}
     </li>
   );
 };
 
 const NavBar = () => {
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
+  };
+
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
-          <img src={images.Logo} alt="logo" />
+          <img className="w-24 lg:w-auto" src={images.Logo} alt="logo" />
         </div>
-        <div className="flex gap-x-9 items-center font-semibold">
-          <ul className="flex gap-x-2 items-center">
+        <div className="lg:hidden z-50">
+          {navIsVisible ? (
+            <IoClose className="w-6 h-6" onClick={navVisibilityHandler} />
+          ) : (
+            <IoMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+          )}
+        </div>
+        <div
+          className={`${
+            navIsVisible ? "right-0" : "-right-full"
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center font-semibold`}
+        >
+          <ul className="text-white lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 items-center gap-y-5">
             {navItemInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
-          <button className="border-2 border-purple-500 px-6 py-2 rounded-full text-purple-500 font-semibold hover:bg-purple-500 hover:text-white transition-all duration-300">
+          <button className="mt-5 lg:mt-0 border-2 border-purple-500 px-6 py-2 rounded-full text-purple-500 font-semibold hover:bg-purple-500 hover:text-white transition-all duration-300">
             Sign in
           </button>
         </div>
